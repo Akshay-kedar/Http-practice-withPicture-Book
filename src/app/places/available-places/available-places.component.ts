dd2d2bcaa1ca3b02f5018dd659d376288df2ad5e
Akshay Kedar <akshay.kedar@das.cpa.com>
2025-03-07 19:37:10 +0530

    adding functionality of Get and Put call

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -1,8 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnDestroy, OnInit, signal } from '@angular/core';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, Subscription, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -11,6 +13,41 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit , OnDestroy {
+
   places = signal<Place[] | undefined>(undefined);
+subscription:Subscription | undefined;
+  constructor(private http: HttpClient) {}
+ isFetching=signal(false);
+error=signal('')
+
+  ngOnInit(): void {
+    this.isFetching.set(true);
+    this.subscription=this.http.get<{places:Place[]}>('http://localhost:3000/places').subscribe(
+      {
+        next:(resData)=>{this.places.set(resData.places)},
+        complete:()=> {
+          this.isFetching.set(false)
+        },
+        error:(error:Error)=>{
+          this.error.set(error.message);
+        }
+      })
+    
+  }
+  onSelectPlaces(selectedPlace: Place) {
+    this.http.put('http://localhost:3000/user-places',{
+      placeId:selectedPlace.id
+    }).subscribe({
+      next:(resData)=>console.log(resData)
+    });
+    }
+
+
+
+  ngOnDestroy(): void {
+    if(this.subscription){
+      this.subscription.unsubscribe
+    }
+  }
 }
